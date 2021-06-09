b0401c1e585f037235088afc51782ae2f75bc19a
subinkim26 <dpaptlove@gmail.com>
2021-06-09 11:28:27 +0900

    loginpage create

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app){
         '/api',
         proxy({
             target : 'http://localhost:5000',
-            changeOrigin : true,
+            changeOrigin : true
         })
     );
 };
