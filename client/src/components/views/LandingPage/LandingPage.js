1a7643e3c8e67e0973e7e9ce5bc35f74f8bb020a
subinkim26 <dpaptlove@gmail.com>
2021-06-10 11:36:30 +0900

    logout page & 작업

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,8 @@
 import React,{useEffect} from 'react'
 import axios from 'axios';
+//import { response } from 'express';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(()=>{
         axios.get('/api/hello')
@@ -9,12 +10,30 @@ function LandingPage() {
 
     }, [])
 
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+        .then(response => {
+           if(response.data.success){
+               props.history.push("/login")
+           }else{
+               alert('로그아웃 실패')
+           }
+
+        })
+    }
+
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height: '100vh'
         }}>
            <h2> 시작 페이지</h2>
+
+
+           <button onClick={onClickHandler}>
+               로그아웃
+           </button>
         </div>
     )
 }
